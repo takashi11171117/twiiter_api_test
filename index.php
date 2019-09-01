663a0ecb525b20cbea634827afac9e7ec785dee2
Saito Takashi <takashi11171117@gmail.com>
2019-09-01 13:46:08 +0900

    Update

--- index.php
+++ index.php
@@ -14,3 +14,29 @@ $account = $connect->get(
 );
 
 var_dump($account);
+
+try {
+    $media1 = $connect->upload('media/upload', ['media' => 'download.jpg']);
+    $result = $connect->post(
+        'statuses/update',
+        [
+            "status" => "twitter apiの画像投稿testだよ",
+            'media_ids' => implode(
+                ',',
+                [
+                    $media1->media_id_string,
+                ]
+            )
+        ]
+    );
+} catch(Exception $e) {
+    var_dump($e);
+}
+
+if($connect->getLastHttpCode() == 200) {
+    // ツイート成功
+    print "tweeted\n";
+} else {
+    // ツイート失敗
+    print "tweet failed\n";
+}
